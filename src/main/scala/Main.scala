5084c456d0d54856b8ec1878d1cdea809529c834
LPignata <luanpignata@aluno.unb.br>
2018-04-11 17:04:45 -0300

    trabalho final

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,21 @@
+import br.unb.cic.epl._
+
 object Main extends App {
-  println("Hello, World!")
+  val lit100 = new Literal(100)
+  val lit500 = new Literal(500)
+
+  println(lit100.print())
+  println(lit500.print())
+
+  val eval = new Eval
+  val add = new Add(lit100, lit500)
+  val sub = new Sub(lit100, lit500)
+  val mult = new Mult(lit100, lit500)
+
+  eval.visit(add)
+  println(add.print() + " = " + eval.result())
+  eval.visit(sub)
+  println(sub.print() + " = " + eval.result())
+  eval.visit(mult)
+  println(mult.print() + " = " + eval.result())
 }
